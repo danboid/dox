5312ae9c8fe757d3aae4cf06b6a1512af54b4b35
Dan MacDonald <allcoms@gmail.com>
2022-10-14 21:11:04 +0100

    Update mkzfshome.sh to copy /etc/skel files into users home directory

--- LTSP/mkzfshome.sh
+++ LTSP/mkzfshome.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [ "$PAM_USER" != "lightdm" ] && [ ! -d "/home/$PAM_USER" ] ; then
         zfs create -o mountpoint=/home/$PAM_USER astarray/home/$PAM_USER
-        chown $PAM_USER:1001 /home/$PAM_USER
+        chown $PAM_USER:$PAM_USER /home/$PAM_USER
         chmod go-rwx /home/$PAM_USER
         zfs set refquota=30G astarray/home/$PAM_USER
+        cp -r /etc/skel/. /home/$PAM_USER
+        chown $PAM_USER:$PAM_USER /home/$PAM_USER/.bashrc \
+        /home/$PAM_USER/.bash_logout /home/$PAM_USER/.profile
 fi
